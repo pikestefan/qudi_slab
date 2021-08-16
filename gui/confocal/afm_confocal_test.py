dcad7f3260d2f399d688352201cf3e9ff8de3af1
pikestefan <lucio.stefan@gmail.com>
2021-08-16 09:41:14 +0200

    Create afm_confocal_test.py
    UI module to test the user interface. To be renamed and edited in the future.

---
+++ gui/confocal/afm_confocal_test.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file contains the Qudi GUI for general Confocal control.
+
+Qudi is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Qudi is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Qudi. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
+top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
+"""
+
+import numpy as np
+import os
+import pyqtgraph as pg
+import time
+
+from core.connector import Connector
+from core.configoption import ConfigOption
+from core.statusvariable import StatusVar
+from qtwidgets.scan_plotwidget import ScanImageItem
+from gui.guibase import GUIBase
+from gui.guiutils import ColorBar
+from gui.colordefs import ColorScaleInferno
+from gui.colordefs import QudiPalettePale as palette
+from gui.fitsettings import FitParametersWidget
+from qtpy import QtCore
+from qtpy import QtGui
+from qtpy import QtWidgets
+from qtpy import uic
+
+
+class ConfocalMainWindow(QtWidgets.QMainWindow):
+    """ Create the Mainwindow based on the corresponding *.ui file. """
+
+    sigPressKeyBoard = QtCore.Signal(QtCore.QEvent)
+    sigDoubleClick = QtCore.Signal()
+
+    def __init__(self):
+        # Get the path to the *.ui file
+        this_dir = os.path.dirname(__file__)
+        ui_file = os.path.join(this_dir, 'ui_afm_cfm.ui')
+        self._doubleclicked = False
+
+        # Load it
+        super(ConfocalMainWindow, self).__init__()
+        uic.loadUi(ui_file, self)
+        self.show()
+
+    def keyPressEvent(self, event):
+        """Pass the keyboard press event from the main window further. """
+        self.sigPressKeyBoard.emit(event)
+
+    def mouseDoubleClickEvent(self, event):
+        self._doubleclicked = True
+        self.sigDoubleClick.emit()
+
+class ConfocalTestGui(GUIBase):
+    """ Main Confocal Class for xy and depth scans.
+    """
+
+    # declare connectors
+    confocallogic1 = Connector(interface='ConfocalLogic')
+
+    default_meter_prefix = ConfigOption('default_meter_prefix', None)  # assume the unit prefix of position spinbox
+
+    # signals
+    sigStartOptimizer = QtCore.Signal(list, str)
+
+    def __init__(self, config, **kwargs):
+        super().__init__(config=config, **kwargs)
+
+    def on_activate(self):
+        """ Initializes all needed UI files and establishes the connectors.
+
+        This method executes the all the inits for the differnt GUIs and passes
+        the event argument from fysom to the methods.
+        """
+
+        # Getting an access to all connectors:
+        self._scanning_logic = self.confocallogic1()
+
+        self._hardware_state = True
+
+        self.initMainUI()      # initialize the main GUI
+
+    def initMainUI(self):
+        """ Definition, configuration and initialisation of the confocal GUI.
+
+        This init connects all the graphic modules, which were created in the
+        *.ui file and configures the event handling between the modules.
+        Moreover it sets default values.
+        """
+        self._mw = ConfocalMainWindow()
+
+        # All our gui elements are dockable, and so there should be no "central" widget.
+        self._mw.centralwidget.hide()
+        self._mw.setDockNestingEnabled(True)
+
+        # always use first channel on startup, can be changed afterwards
+        self.xy_channel = 0
+        self.depth_channel = 0
+        self.opt_channel = 0
+
+        ###################################################################
+        #               Configuring the dock widgets                      #
+        ###################################################################
+        # All our gui elements are dockable, and so there should be no "central" widget.
+
+        self.show()
+
+    def on_deactivate(self):
+        """ Reverse steps of activation
+
+        @return int: error code (0:OK, -1:error)
+        """
+        self._mw.close()
+        return 0
+
+    def show(self):
+        """Make main window visible and put it above all other windows. """
+        # Show the Main Confocal GUI:
+        self._mw.show()
+        self._mw.activateWindow()
+        self._mw.raise_()
