ebe626fe1a9798d47200a54f18f3277018cc50c8
pikestefan <lucio.stefan@gmail.com>
2022-02-08 14:04:44 +0100

    Corrected small mistake

--- logic/snvm_logic.py
+++ logic/snvm_logic.py
@@ -220,7 +220,7 @@ class SnvmLogic(GenericLogic):
         self.signal_continue_snvm.emit()
 
     def continue_snvm_scanning(self):
-        acquire_data = False if (self.store_retrace is False) and (self._is_retracing is True) else False
+        acquire_data = False if (self.store_retrace is False) and (self._is_retracing is True) else True
         if acquire_data:
             #If the index of the ODMR is less than the averages, keep acquiring
             if self._odmr_rep_index < self.odmr_averages:
